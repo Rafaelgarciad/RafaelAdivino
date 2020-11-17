fa68b8b5e18047e580759f1f5257641936a12893
garri <garri@10.0.2.15>
2020-11-17 16:15:37 +0100

    Valores 10 al 50

--- RafaelAdivino/src/Adivino/Aciertos.java
+++ RafaelAdivino/src/Adivino/Aciertos.java
@@ -5,17 +5,27 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (Math.random()*50+10);
+        
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
+        
         do {
-            System.out.print("\nIntroduce el valor de X: ");
+            System.out.print("\nIntroduce un valor entre 10 y 50: ");
             n = sc.nextInt();
-            if (n > x) {
+            
+            if (n < 10) {
+                System.out.print("Valor minimo 10");
+            } else if (n > 50){
+                System.out.print("Valor maximo 50");
+            }
+            
+            if (n > x && n <= 50) {
                 System.out.print("Te pasaste Amigo");
-            } else if (n < x){
+            } else if (n < x && n >= 10){
                 System.out.print("Casi cerca ...");
             }
+            
             cont++;
         } while (n != x);
         System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
